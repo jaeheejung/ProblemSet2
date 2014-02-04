e2337af0a7b299e4877e28884fb09a7757fc6a0e
jaeheejung <jaeheejung@wustl.edu>
2014-02-04 02:13:56 -0600

    PS2Jung
    Code for questions 1, 2, and 3-1

--- R.PS2.Jung.R
+++ R.PS2.Jung.R
@@ -3,7 +3,7 @@
 
 
 #1
-violations <- function(votes.tot,statistic){
+benfords <- function(votes.tot,statistic){
 	#Argument "votes.tot" takes in a vector or matrix of observed vote totals
 	#Argument "statistic" specifies the statistic you want to calculate as we will see below
 	prop.freq <- numeric(9) #Creates a numeric vector of length nine
@@ -42,8 +42,8 @@ return(list(digit.distribution=prop.freq,d.statistic=sqrt(length(votes.tot))*sqr
 #2
 print.benfords <- function(votes.tot,statistic="both"){
 	#By default, this function calculates both statistics
-	stat.list <- violations(votes.tot,statistic)[-1]
-	#Excludes the first element of the list obtained from violations() because we do not need the digit distribution anymore
+	stat.list <- benfords(votes.tot,statistic)[-1]
+	#Excludes the first element of the list obtained from benfords() because we do not need the digit distribution anymore
 	stat.table <- as.matrix(stat.list)
 	#Transforms the list to a matrix, which is more like a table
 	colnames(stat.table) <- "Value"
@@ -65,3 +65,67 @@ print.benfords <- function(votes.tot,statistic="both"){
 	cat("Significance level: 0.10*, 0.05**, 0.01***")
 	#A line included to explain what the stars mean
 }
+
+
+#3
+unittest.benfords <- function(){
+	data1 <- rep(seq(100,900,by=100),round(log10(1+(1/(1:9)))*100))
+	#data1 is artifical data that meets Benford's law
+	data1.distribution <- numeric(9)
+	for(i in 1:9){ 
+		data1.distribution[i] <- sum((substr(data1,start=1,stop=1)==i)*1)/length(data1)
+	}
+	#Calculates distribution of proportional frequencies of integers in data1
+	data1.m <- numeric(9)
+	for(i in 1:9){
+		data1.m[i] <- abs(data1.distribution[i]-log10(1+(1/i)))
+	}
+	data1.m <- sqrt(length(data1))*max(data1.m)
+	#data1.m is the m statistic calculated for data1
+	data1.d <- numeric(9)
+	for(i in 1:9){
+		data1.d[i] <- (data1.distribution[i]-log10(1+(1/i)))^2
+	}
+	data1.d <- sqrt(length(data1))*sqrt(sum(data1.d))
+	#data1.d is the d statistic calculated for data1
+	benfords.data1 <- benfords(data1,"both")
+	#benfords.data1 is the output of data1 using benfords(), which is the subject of unit testing
+	data2 <- rep(seq(100,900,by=100),round(log10(1+(1/(9:1)))*100))
+	#data2 is artifical data that doesn't meet Benford's law
+	data2.distribution <- numeric(9)
+	for(i in 1:9){ 
+		data2.distribution[i] <- sum((substr(data2,start=1,stop=1)==i)*1)/length(data2)
+	}
+	#Calculates distribution of proportional frequencies of integers in data2
+	data2.m <- numeric(9)
+	for(i in 1:9){
+		data2.m[i] <- abs(data2.distribution[i]-log10(1+(1/i)))
+	}
+	data2.m <- sqrt(length(data2))*max(data2.m)
+	#data2.m is the m statistic calculated for data2
+	data2.d <- numeric(9)
+	for(i in 1:9){
+		data2.d[i] <- (data2.distribution[i]-log10(1+(1/i)))^2
+	}
+	data2.d <- sqrt(length(data2))*sqrt(sum(data2.d))
+	#data2.d is the d statistic calculated for data2
+	benfords.data2 <- benfords(data2,"both")
+	#benfords.data2 is the output of data2 using benfords(), which is the subject of unit testing
+	matches <- c(benfords.data1[[1]]==data1.distribution, benfords.data1[[2]]==data1.m, benfords.data1[[3]]==data1.d, benfords.data2[[1]]==data2.distribution, benfords.data2[[2]]==data2.m, benfords.data2[[3]]==data2.d)
+	#Creates a logical vector that shows whether or not the true calculations for each data are equal to the corresponding values in the outputs from benfords() 
+	if(all(matches)==TRUE){
+		cat("TRUE")
+	}else{
+		cat("FALSE\n\n")
+		}
+	#Returns TRUE if there is perfect match between the true calculations and the outputs of benfords(), and returns FALSE otherwise
+	if(any(benfords.data1[[1]]!=data1.distribution)==TRUE){
+		cat('Distribution for data1 wrong.\n')
+	}
+	if(any(benfords.data2[[1]]!=data2.distribution)==TRUE){
+		cat('Distribution for data2 wrong.')
+	}
+	#If there is no perfect match, specifies what went wrong. Since I specified the benfords function to calculate both m and d statistics in the function argument, there is no need to consider errors in which the benfords function calculates only either one of the two statistics.
+}
+
+	
